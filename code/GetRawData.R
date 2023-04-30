d17055eb602ec37302f4b93234def7a365896d87
rekkasa <a.rekkas@erasmusmc.nl>
2023-04-30 18:47:45 +0200

    simplify Makefile

--- code/GetRawData.R
+++ code/GetRawData.R
@@ -1,52 +1,35 @@
 #!/usr/bin/env Rscript
 
 args = commandArgs(trailingOnly = TRUE)
-resultFile <- args[1]
-chapter <- args[2]
-prefix <- args[3]
+resultsLocation <- args[1]
 
 message(
-  paste("Fetching", resultFile, "from github")
+  paste("Fetching", resultsLocation, "from github")
 )
 
 githubAccount <- "https://github.com/rekkasa"
 repo <- "phd_thesis"
 repoLocation <- file.path(
-  "raw/large-files/data",
-  chapter
+  "raw/large-files",
+  resultsLocation
 )
 
 repo <- file.path(githubAccount, repo, repoLocation)
 
-if (!is.na(prefix)) {
-  filePath <- file.path(
-    repo,
-    prefix,
-    resultFile
-  )
-} else {
-  filePath <- file.path(repo, resultFile)
-}
-
-saveDirectory = file.path("data", chapter)
-if (!is.na(prefix)) {
-  saveDirectory <- file.path(saveDirectory, prefix)
-}
-
-if (!dir.exists(saveDirectory)) {
-  dir.create(saveDirectory, recursive = TRUE)
+if (!dir.exists(dirname(resultsLocation))) {
+  dir.create(dirname(resultsLocation), recursive = TRUE)
 }
 
 saveRDS(
-  readRDS(url(filePath)),
-  file = file.path(saveDirectory, resultFile)
+  readRDS(url(repo)),
+  file = resultsLocation
 )
 
 message(
   crayon::green(
     paste(
       "\u2713 File saved at:",
-      file.path(file.path(saveDirectory, resultFile)),
+      resultsLocation,
       "\n"
     )
   )
