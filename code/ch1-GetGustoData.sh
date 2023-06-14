a5dae96f2d8054220ce592a09e06162cabbf65bc
rekkasa <a.rekkas@erasmusmc.nl>
2023-06-14 14:38:00 +0200

    use https when fetching gusto data

--- code/ch1-GetGustoData.sh
+++ code/ch1-GetGustoData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [[ ! -e "data/introduction" ]] && mkdir -p data/introduction || echo -e "Directory introduction already exists"
-[[ ! -f "data/introduction/gusto.rda" ]] && wget --output-document data/introduction/gusto.rda -nc http://hbiostat.org/data/gusto.rda || echo -e "Nothing to be done"
+[[ ! -f "data/introduction/gusto.rda" ]] && wget --output-document data/introduction/gusto.rda -nc https://hbiostat.org/data/gusto.rda || echo -e "Nothing to be done"
